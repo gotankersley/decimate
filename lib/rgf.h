aea56fdfc67396b2f5cc7155bf90534955584bee
gotankersley@harding.edu <gotankersley@harding.edu>
2025-12-15 10:35:46 -0600

    More io_lib cleanup

--- lib/rgf.h
+++ lib/rgf.h
@@ -12,8 +12,6 @@ void gen_rgf_row_old(int n, int k, uint8_t& CUR, fmpz_mat_t rowOut); //DEPRECATE
 void gen_rgf_row(int n, int k, fmpz_mat_t rowOut); 
 void gen_rgf_cell(int n, int k, int level, fmpz_mat_t rowOut); 
 	
-//void serialize_mat(const char* filename, fmpz_mat_t mat);
-//void deserialize_mat(const char* filename, fmpz_mat_t mat); 
 
 void rgf_rank(std::vector<uint8_t>& rgf, int k, fmpz_t rankOut);
 void rgf_rank_table(std::vector<uint8_t>& rgf, int k, fmpz_mat_t table, fmpz_t rankOut); //Use Precomputed Table
